c213e73065e8a64b537bcdf8f0b56d919f7ea84e
Марьян Марков <alchnichort@gmail.com>
2023-02-04 12:23:58 +0300

    fc

---
+++ T1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.Write(" От ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write(" До ");
+int n = int.Parse(Console.ReadLine()!);
+int summ = 0;
+for (int i = m; i < n + 1; i++)
+{
+    if (i > 0)
+    {
+        summ = summ + i;
+    }
+}
+Console.WriteLine($"Сумма натуральных элементов от {m} до {n} равна {summ}");
---
+++ T2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n
+Console.Write(" m = ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write(" n = ");
+int n = int.Parse(Console.ReadLine()!);
+
+int Ack(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
+}
+
+Console.Write("Функция Аккермана = ");
+Console.Write(Ack(m,n));
